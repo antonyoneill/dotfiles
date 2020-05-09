e41aaf8d39ebb73c19751d240cdc728689fff407
Antony O'Neill <antony@ant-web.co.uk>
2020-05-09 15:25:56 +0100

    Quote subshell

--- zsh/.zsh/04-gpg.zsh
+++ zsh/.zsh/04-gpg.zsh
@@ -8,5 +8,5 @@ gpg-connect-agent updatestartuptty /bye >/dev/null
 # Allow remote machines to access the local agent
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
