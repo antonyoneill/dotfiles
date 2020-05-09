9ce5484e0ff3937a370a4f6bea398e729b4d454c
Antony O'Neill <antony@ant-web.co.uk>
2020-05-09 15:41:34 +0100

    Fix order of gpg-agent commands

--- zsh/.zsh/04-gpg.zsh
+++ zsh/.zsh/04-gpg.zsh
@@ -1,12 +1,12 @@
-# Set up GPG agent for use in the new shell
-
-export GPG_TTY="$(tty)"
-
+# Start the agent
 gpgconf --launch gpg-agent
-gpg-connect-agent updatestartuptty /bye >/dev/null
 
 # Allow remote machines to access the local agent
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
+
+# Set up GPG agent for use in the new shell
+export GPG_TTY="$(tty)"
+gpg-connect-agent updatestartuptty /bye >/dev/null
