33c4176851f64522f4e69f6e14fddc9fe67e7e02
Antony O'Neill <antony@ant-web.co.uk>
2020-06-21 12:45:05 +0100

    Remove gpg forwarded if it is old

--- zsh/.zsh/04-gpg.zsh
+++ zsh/.zsh/04-gpg.zsh
@@ -1,8 +1,19 @@
 if ! command -v gpgconf >/dev/null 2>&1
 then
 	echo "gpgconf not installed"
-elif (( ! ${+SSH_CLIENT} ))
+elif (( ${+SSH_CLIENT} ))
 then
+    agent_path="/run/user/$(id -u)/gnupg/S.gpg-agent"
+
+    old_agent="$(find $agent_path -not -mmin -2)"
+
+    if [[ -n "$old_agent"  && "$SHLVL" -eq 1 ]]
+    then
+        echo "Cleaning old forwarded agent. Reconnect"
+        rm -rf "$agent_path"
+        exit
+    fi
+else
  	# Allow remote machines to access the local agent
 	unset SSH_AGENT_PID
 	if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
