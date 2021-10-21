2026d36c76293adf94189b83919174e347157a78
Antony O'Neill <antony@ant-web.co.uk>
2021-10-21 13:29:19 +0100

    WSL gpg magic

--- zsh/.zsh/04-gpg.zsh
+++ zsh/.zsh/04-gpg.zsh
@@ -1,6 +1,24 @@
 if ! command -v gpgconf >/dev/null 2>&1
 then
 	echo "gpgconf not installed"
+elif (( ${+WSLENV} ))
+then
+	# SSH Socket
+	# Removing Linux SSH socket and replacing it by link to wsl2-ssh-pageant socket
+	export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock 
+	ss -a | grep -q $SSH_AUTH_SOCK 
+	if [ $? -ne 0 ]; then
+		  rm -f $SSH_AUTH_SOCK
+		    setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/.ssh/wsl2-ssh-pageant.exe" &>/dev/null &!
+	fi
+	# GPG Socket
+	# Removing Linux GPG Agent socket and replacing it by link to wsl2-ssh-pageant GPG socket
+	export GPG_AGENT_SOCK=$HOME/.gnupg/S.gpg-agent 
+	ss -a | grep -q $GPG_AGENT_SOCK 
+	if [ $? -ne 0 ]; then
+		  rm -rf $GPG_AGENT_SOCK
+		    setsid nohup socat UNIX-LISTEN:$GPG_AGENT_SOCK,fork EXEC:"$HOME/.ssh/wsl2-ssh-pageant.exe --gpg S.gpg-agent" &>/dev/null &!
+	fi
 elif (( ${+SSH_CLIENT} ))
 then
     agent_path="/run/user/$(id -u)/gnupg/S.gpg-agent"
